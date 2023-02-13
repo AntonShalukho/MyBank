91cb9f02f1b20b575a1f7b00a68592c3b0fe5f3a
Anton Shalukho <antonshalukho@gmail.com>
2023-02-13 14:43:46 +0100

    prepared empty url

--- front/src/urls.ts
+++ front/src/urls.ts
@@ -1,78 +1,53 @@
-export const PASSPORT_NUMBER_URL =
-  "https://abank-auth-ms.andersenlab.dev/a-banking/recovery-management/password-recovery/verification/credentials/passport";
+export const PASSPORT_NUMBER_URL = "";
 
-export const TELEGRAM_REQUEST_URL =
-  "https://abank-auth-ms.andersenlab.dev/a-banking/recovery-management/user/verification-code/new-code";
+export const TELEGRAM_REQUEST_URL = "";
 
-export const REGISTRATION_TELEGRAM_URL =
-  "https://abank-registration-ms.andersenlab.dev/a-banking/registration-management/user/verification-code/new-code";
+export const REGISTRATION_TELEGRAM_URL = "";
 
-export const VERIFICATION_CODE_URL =
-  "https://abank-verification-code-ms.andersenlab.dev/a-banking/verification-management/validation/is-expired";
+export const VERIFICATION_CODE_URL = "";
 
-export const LOGIN_REQUEST_URL =
-  "https://abank-auth-ms.andersenlab.dev/a-banking/auth/login";
+export const LOGIN_REQUEST_URL = "";
 
-export const PHONE_NUMBER_URL =
-  "https://abank-registration-ms.andersenlab.dev/a-banking/registration-management/verification/credentials/phone-number";
+export const PHONE_NUMBER_URL = "";
 
-export const CONFIDENTIALITY_POLICY_URL =
-  "https://wiki.andersenlab.com/pages/viewpage.action?pageId=193740598";
+export const CONFIDENTIALITY_POLICY_URL = "";
 
-export const ONLINE_BANKING_SERVICES_URL =
-  "https://wiki.andersenlab.com/pages/viewpage.action?pageId=193740654";
+export const ONLINE_BANKING_SERVICES_URL = "";
 
 export const GOOGLE_PLAY_STORE_URL = "https://play.google.com/store";
 
 export const APPLE_APP_STORE_URL = "https://www.apple.com/store";
 
-export const REGISTRATION_CLIENT_URL =
-  "https://abank-registration-ms.andersenlab.dev/a-banking/registration-management/registration/client";
+export const REGISTRATION_CLIENT_URL = "";
 
-export const REGISTRATION_NON_CLIENT_URL =
-  "https://abank-registration-ms.andersenlab.dev/a-banking/registration-management/registration/non-client";
+export const REGISTRATION_NON_CLIENT_URL = "";
 
-export const EXCHANGE_RATES_URL =
-  "https://abank-currency-rates-ms.andersenlab.dev/a-banking/exchange-rates?date=";
+export const EXCHANGE_RATES_URL = "";
 
-export const PASSWORD_RECOVERY_URL =
-  "https://abank-auth-ms.andersenlab.dev/a-banking/recovery-management/password-recovery/by-passport/by-code";
+export const PASSWORD_RECOVERY_URL = "";
 
-export const PROFILE_PICTURE_URL =
-  "https://abank-user-management-ms.andersenlab.dev/a-banking/profile/settings/photo";
+export const PROFILE_PICTURE_URL = "";
 
-export const ACCOUNT_INFO_URL =
-  "https://abank-user-management-ms.andersenlab.dev/a-banking/profile/general-information";
+export const ACCOUNT_INFO_URL = "";
 
-export const CHANGE_PASSWORD_URL =
-  "https://abank-user-management-ms.andersenlab.dev/a-banking/profile/security-management/password";
+export const CHANGE_PASSWORD_URL = "";
 
-export const CHANGE_QUESTION_URL =
-  "https://abank-user-management-ms.andersenlab.dev/a-banking/profile/security-management/secret-question";
+export const CHANGE_QUESTION_URL = "";
 
-export const NOTIFICATIONS_URL =
-  "https://abank-user-management-ms.andersenlab.dev/a-banking/profile/notifications";
+export const NOTIFICATIONS_URL = "";
 
-export const SET_NOTIFICATIONS_STATUS =
-  "https://abank-user-management-ms.andersenlab.dev/a-banking/profile/notifications/";
+export const SET_NOTIFICATIONS_STATUS = "";
 
-export const GET_CITIES_DATA =
-  "https://abank-atm-ms.andersenlab.dev/a-banking/cities?city=";
+export const GET_CITIES_DATA = "";
 
-export const GET_CARDS_DATA =
-  "https://abank-card-ms.andersenlab.dev/a-banking/cards";
+export const GET_CARDS_DATA = "";
 
-export const GET_TRANSACTION_HISTORY_DATA =
-  "https://abank-card-ms.andersenlab.dev/a-banking/transactions/history?pageNumber=0&pageSize=3";
+export const GET_TRANSACTION_HISTORY_DATA = "";
 
-export const GET_EMAIL_VERIFICATION_CODE_URL =
-  "https://abank-user-management-ms.andersenlab.dev/a-banking/profile/email-management/verification-code?email=";
+export const GET_EMAIL_VERIFICATION_CODE_URL = "";
 
-export const CHANGE_EMAIL_URL =
-  "https://abank-user-management-ms.andersenlab.dev/a-banking/profile/email-management/email";
+export const CHANGE_EMAIL_URL = "";
 
-export const GET_LOANS_PRODUCTS =
-  "https://abank-loan-ms.andersenlab.dev/a-banking/loans/products";
+export const GET_LOANS_PRODUCTS = "";
 
-export const GET_LOANS_PRODUCTS_DESCRIPTION =
-  "https://abank-loan-ms.andersenlab.dev/a-banking/loans/products/details?loanName=";
+export const GET_LOANS_PRODUCTS_DESCRIPTION = "";
